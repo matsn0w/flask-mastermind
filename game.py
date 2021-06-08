b654c10cb6b55e0d3c24cd309a933927a10dea0f
matsn0w <bartscholtus@gmail.com>
2021-06-08 15:16:08 +0200

    fix error handling

--- game.py
+++ game.py
@@ -5,29 +5,32 @@ class Game:
         self.__MIN_POSITIONS = 4
         self.__MAX_POSITIONS = 10 
 
-        self._player = player
-        self._doubles = doubles
-        self._colors = colors
-        self._position = position
+        self.player = player
+        self.doubles = doubles
+        self.colors = colors
+        self.position = position
 
     @property
-    def _colors(self):
+    def colors(self):
         return self._colors
 
-    @_colors.setter
-    def _colors(self, value):
-        if (value < self.__MIN_COLORS or value > self.__MAX_COLORS):
-            raise Exception(f'The amount of colors must be between {self.__MIN_COLORS} and {self.__MAX_COLORS}!')
+    @colors.setter
+    def colors(self, value):
+        if value < self.__MIN_COLORS or value > self.__MAX_COLORS:
+            raise ValidationError(f'The amount of colors must be between {self.__MIN_COLORS} and {self.__MAX_COLORS}!')
 
-        self._colors = value
+        pass
 
     @property
-    def _position(self):
+    def position(self):
         return self._position
 
-    @_position.setter
-    def _position(self, value):
-        if (value < self.__MIN_POSITIONS or value > self.__MAX_POSITIONS):
-            raise Exception(f'The amount of positions must be between {self.__MIN_POSITIONS} and {self.__MAX_POSITIONS}!')
+    @position.setter
+    def position(self, value):
+        if value < self.__MIN_POSITIONS or value > self.__MAX_POSITIONS:
+            raise ValidationError(f'The amount of positions must be between {self.__MIN_POSITIONS} and {self.__MAX_POSITIONS}!')
 
-        self._position = value
+        pass
+
+class ValidationError(Exception):
+    pass
