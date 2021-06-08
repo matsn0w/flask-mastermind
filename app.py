3ba5fc164e62bfe7e15926f6720d7fc1a244c089
matsn0w <bartscholtus@gmail.com>
2021-06-08 15:16:19 +0200

    add game creation logic

--- app.py
+++ app.py
@@ -1,9 +1,26 @@
-from player import Player
-from game import Game
 from flask import Flask, render_template, request
+from player import Player
+from game import Game, ValidationError
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=['GET'])
 def index():
     return render_template('index.html')
+
+@app.route('/', methods=['POST'])
+def start_game():
+    # extract form data
+    player = Player(request.form['player_name'])
+    doubles = True if request.form['use_doubles'] == 'on' else False
+    colors = int(request.form['amount_colors'])
+    positions = int(request.form['amount_positions'])
+
+    try:
+        # create a new game
+        game = Game(player, doubles, colors, positions)
+    except ValidationError as e:
+        return render_template('index.html', errors=e.args)
+
+    # show the game screen
+    return render_template('game.html', game=game)
