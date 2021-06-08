e5fb11171aeeb8a5758632cb5cdd24bc5c785e9c
matsn0w <bartscholtus@gmail.com>
2021-06-08 14:46:47 +0200

    fix naming

--- game.py
+++ game.py
@@ -11,23 +11,23 @@ class Game:
         self._position = position
 
     @property
-    def colors(self):
+    def _colors(self):
         return self._colors
 
-    @colors.setter
-    def colors(self, value):
+    @_colors.setter
+    def _colors(self, value):
         if (value < self.__MIN_COLORS or value > self.__MAX_COLORS):
             raise Exception(f'The amount of colors must be between {self.__MIN_COLORS} and {self.__MAX_COLORS}!')
 
-        self.colors = value
+        self._colors = value
 
     @property
-    def position(self):
+    def _position(self):
         return self._position
 
-    @position.setter
-    def position(self, value):
+    @_position.setter
+    def _position(self, value):
         if (value < self.__MIN_POSITIONS or value > self.__MAX_POSITIONS):
             raise Exception(f'The amount of positions must be between {self.__MIN_POSITIONS} and {self.__MAX_POSITIONS}!')
 
-        self.position = value
+        self._position = value
